0a9af9d965fff83a92813f90255c6ca44f01229d
pratheeshdev <pratheeshdev@gmail.com>
2023-03-07 01:18:43 -0800

    changed terraform rg

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
-  #source  = "app.terraform.io/pdevorg/vpc/aws"
-  source  = "github.com/pratheeshdev/terraform-aws-vpc"
-  #version = "1.0.0"
+  source  = "app.terraform.io/pdevorg/vpc/aws"
+  #source  = "github.com/pratheeshdev/terraform-aws-vpc"
+  version = "1.0.0"
   vpc_cidr          = var.vpc_cidr
   public_subnet     = var.public_subnet
   private_subnet    = var.private_subnet
